7ede14f1931f5012dba536764816e57145818c2a
Matej Roguljic <matej.roguljic@gmail.com>
2024-01-16 14:10:38 +0100

    TPrimePair config

--- one_step/multi_crab_submit_onestep.py
+++ one_step/multi_crab_submit_onestep.py
@@ -3,8 +3,8 @@ import os
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', type=str, default='HbbGamma_JHUGen_pythia8', help='name of dataset')
-    parser.add_argument('--config', type=str, default='Hbb_fragment',  help='config of dataset')
+    parser.add_argument('--name', type=str, default='TPrimePairProd_MG5_Pythia8', help='name of dataset')
+    parser.add_argument('--config', type=str, default='B2G-RunIISummer20UL16wmLHEGEN-02702-fragment',  help='config of dataset')
     parser.add_argument('--eosdir', type=str, default='/store/group/phys_higgs/hbb/matej/', help='eosdir')
     parser.add_argument('--site', type=str, default='T2_CH_CERN', help='site')
     parser.add_argument('--begin-seed', type=int, default=0, help='begin seed, should be between 0 and 99')
@@ -27,13 +27,13 @@ if __name__ == '__main__':
     config.JobType.pluginName = 'PrivateMC'
 
     config.section_("Data")
-    nevent = 1000
+    nevent = 100
     config.Data.inputDBS = 'global'
     config.Data.splitting = 'EventBased'
     config.Data.unitsPerJob = nevent
-    config.Data.totalUnits = 150000
+    config.Data.totalUnits = 10000
     #config.Data.totalUnits = 5 # for testing
-    config.Data.publication = True
+    config.Data.publication = False
     config.Data.allowNonValidInputDataset = True
 
     config.section_("Site")
@@ -53,14 +53,15 @@ if __name__ == '__main__':
     ## From now on that's what users should modify: this is the a-la-CRAB2 configuration part. ##
     #############################################################################################
     pset_dir = os.environ["CMSSW_BASE"]+"/src"
-    config.General.requestName = 'Private_'+args.name+'_ONESTEP'
+    config.General.requestName = 'Private_'+args.name+'_'+args.year
     config.Data.outputDatasetTag = 'PrivateMC'
     config.Data.outputPrimaryDataset = args.name
     config.Data.outLFNDirBase = args.eosdir + args.name
     config.JobType.allowUndistributedCMSSW = True
     #config.JobType.psetName = 'FAKEMiniAODv2_cfg.py'
     config.JobType.psetName = 'FAKENanoAODv9_cfg.py'
-    config.JobType.inputFiles = ['FrameworkJobReport.xml', 'inputs', 'gammaHiggs0MyyTobb_M125_13TeV_JHUGenV750_pythia8_slc7_amd64_gcc820_CMSSW_10_6_0.tgz']
+    #EDIT THIS
+    config.JobType.inputFiles = ['FrameworkJobReport.xml', 'inputs', 'TT_SStt_slc7_amd64_gcc10_CMSSW_12_4_8_tarball.tar.xz']
     config.JobType.maxMemoryMB = 5000
     config.JobType.numCores = 1
     config.JobType.sendExternalFolder = True
